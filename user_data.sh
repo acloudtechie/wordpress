123f6ef9e4593a9b2b0fbc456e9ab2f0e5da877f
Ming Zhang <ming.zhang@micropact.com>
2017-09-15 13:49:10 -0400

    add playbooks and cloudformation for wordpress installation

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd24 php70 php70-mysqlnd
-sudo service httpd start
-sudo chkconfig httpd on
+#sudo yum update -y
+#sudo yum install -y httpd24 php70 php70-mysqlnd
+#sudo service httpd start
+#sudo chkconfig httpd on
+#test -e /usr/bin/python || sudo apt-get install -y python
+test -e /usr/bin/python || sudo yum install -y python
